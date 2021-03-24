1802e3a8c0758c19891a16a39ad84061cd34ac19
Mariano Gonzalez <56511727+marianoGnlz@users.noreply.github.com>
2021-03-24 12:02:46 -0300

    cosas

---
+++ cliente/src/components/auth/Login.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom'
+
+const Login = () => {
+    const [usuario, setUsuario] = useState({
+        email:'',
+        password: ''
+    })
+
+    const { email, password } = usuario;
+
+
+
+
+    const onChange = e => {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+    const onSubmit = e => {
+        e.preventDefault();
+
+
+
+
+    }
+    return (
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Tu Email"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Tu Password"
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sesión" />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Obtener Cuenta
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
