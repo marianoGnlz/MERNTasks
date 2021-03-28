4f83fa310490fdde82f67accf11d3414357a7e04
Mariano Gonzalez <marianognlz23@gmail.com>
2021-03-28 12:20:09 -0300

    cosas

--- servidor/index.js
+++ servidor/index.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 4000;
 
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
+app.use('/api/tareas', require('./routes/tareas'));
 
 app.listen(PORT, () => {
     console.log(`Servidor disponible en el puerto: ${PORT}`)
